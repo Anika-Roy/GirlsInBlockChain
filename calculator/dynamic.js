1c204c77857097ee4e228104a9d88203fd883ec5
Anika Roy <anika1234roy@gmail.com>
2023-05-22 19:43:23 +0530

    calculator finish:)

--- calculator/dynamic.js
+++ calculator/dynamic.js
@@ -1,6 +1,32 @@
-// let buttons = document.querySelectorAll('.number')
-// buttons.forEach((btn) => {
-//   btn.addEventListener("click", (event) => {
-//     alert(event.target);
-//   });
-// });
+document.addEventListener('DOMContentLoaded', function() {
+    // Select elements
+    const display = document.getElementById('display');
+    const buttons = document.querySelectorAll('.number, .operator, .decimal');
+    const equalButton = document.querySelector('.equal');
+    const clearButton = document.querySelector('.clear');
+    const backButton = document.querySelector('.back');
+
+    // Add event listeners to buttons
+    buttons.forEach(button => {
+      button.addEventListener('click', () => {
+        display.value += button.innerText;
+      });
+    });
+
+    equalButton.addEventListener('click', () => {
+      try {
+        const result = eval(display.value);
+        display.value = result;
+      } catch (error) {
+        display.value = 'Error';
+      }
+    });
+
+    clearButton.addEventListener('click', () => {
+      display.value = '';
+    });
+
+    backButton.addEventListener('click', () => {
+      display.value = display.value.slice(0, -1);
+    });
+  });
